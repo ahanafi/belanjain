95d81a733925700f31d19dc19e6842d1841df3e9
Ahmad Hanafi <Contaminated007@gmail.com>
2021-12-03 06:44:04 +0700

    feat: change login form width and background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,49 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>{{config('app.name')}}</title>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <title>{{config('app.name')}}</title>
 
-  <!-- Template CSS -->
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('stisla-assets/css/components.css')}}">
-  <link rel="stylesheet" href="{{asset('stisla-assets/css/style.css')}}">
-  <link rel="stylesheet" href="{{asset('plugin/datatables/datatables.css')}}">
-  @stack('styles')
+    <!-- Template CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('stisla-assets/css/components.css')}}">
+    <link rel="stylesheet" href="{{asset('stisla-assets/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('plugin/datatables/datatables.css')}}">
+    @stack('styles')
 </head>
 
 <body>
-  <div id="app">
+<div id="app">
     <div class="main-wrapper">
-      <div class="navbar-bg"></div>
+        <div class="navbar-bg"></div>
 
-      @include('layouts.navbar')
-      
-	  <div class="main-sidebar">
-		  @include('layouts.sidebar')
-      </div>
+        @include('layouts.navbar')
 
-      <!-- Main Content -->
-      <div class="main-content">
-        @yield('content')
-      </div>
-      <footer class="main-footer">
-        <div class="footer-left">
-          {{-- Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a> --}}
+        <div class="main-sidebar">
+            @include('layouts.sidebar')
         </div>
-        <div class="footer-right">
-          2.3.0
+
+        <!-- Main Content -->
+        <div class="main-content">
+            @yield('content')
         </div>
-      </footer>
+        <footer class="main-footer">
+            <div class="footer-left">
+                {{-- Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a> --}}
+            </div>
+            <div class="footer-right">
+                2.3.0
+            </div>
+        </footer>
     </div>
-  </div>
+</div>
 
-  <script src="{{asset('js/app.js')}}" ></script>
-  <script src="{{asset('plugin/datatables/datatables.js')}}" ></script>
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('plugin/datatables/datatables.js')}}"></script>
 
-  <script>
-    function deleteModel(deleteUrl, tableId){
+<script>
+    function deleteModel(deleteUrl, tableId) {
         Swal.fire({
             title: "Warning",
             text: "Yakin menghapus data?",
@@ -56,40 +56,40 @@
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajax({
-                    url : deleteUrl,
-                    dataType : "Json",
-                    data : {"_token": "{{ csrf_token() }}"},
-                    method : "delete",
-                    success:function(data){
+                    url: deleteUrl,
+                    dataType: "Json",
+                    data: {"_token": "{{ csrf_token() }}"},
+                    method: "delete",
+                    success: function (data) {
                         console.log(data)
-                        if(data.code == 1){
+                        if (data.code == 1) {
                             Swal.fire(
                                 'Berhasil',
                                 data.message,
                                 'success'
-                        )
-                        }else{
+                            )
+                        } else {
                             Swal.fire({
                                 icon: 'error',
                                 title: 'Oops...',
                                 text: data.message
                             })
                         }
-                        $('#'+tableId).DataTable().ajax.reload();
+                        $('#' + tableId).DataTable().ajax.reload();
                     }
                 })
             }
         })
-      }
+    }
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('select').select2();
     })
-      
-  </script>
 
-  <!-- Page Specific JS File -->
-  {{-- <script src="{{asset('admin/js/page/index.js')}}"></script> --}}
-  @stack('scripts')
+</script>
+
+<!-- Page Specific JS File -->
+{{-- <script src="{{asset('admin/js/page/index.js')}}"></script> --}}
+@stack('scripts')
 </body>
 </html>
