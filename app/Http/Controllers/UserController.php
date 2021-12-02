3975c43deee4624883d9aa555d72931fe75cf836
Ahmad Hanafi <Contaminated007@gmail.com>
2021-12-03 06:29:26 +0700

    feat: remove boilerplate code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,85 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use App\DataTables\UserDataTable;
-use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller{
-
-    public function index(){
-        return view('user.index');
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
     }
 
-    public function datatable(){
-        $users = User::all();
-
-        return UserDataTable::set($users);
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
-    public function create(){
-        $levels = [User::$staff => User::$staff, User::$admin => User::$admin, User::$kades => User::$kades];
-
-        return view('user.create', compact('levels'));
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
     }
 
-    public function store(UserRequest $request){
-        try{
-            User::create($request->validated());
-        }catch(Exception $e){
-            Log::info($e->getMessage());
-            return redirect()->back()->withInput()->with('error', 'Gagal menambahkan data pengguna');
-        }
-
-        return redirect()->route('user.index')->with('success', 'Berhasil menambahkan data pengguna');
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
     }
 
-    public function edit(User $user){
-        $levels = [User::$staff => User::$staff, User::$admin => User::$admin, User::$kades => User::$kades];
-
-        return view('user.edit', compact('user', 'levels'));
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
     }
 
-    public function update(UserRequest $request, User $user){
-        try{
-            $data = $request->all();
-
-            if($data['password']){
-                $data['password'] = bcrypt($data['password']);
-            }else{
-                unset($data['password']);
-            }
-
-            $user->update($data);
-
-            // $base_64_foto = json_decode($request['foto'], true);
-            // $upload_image = uploadFile($base_64_foto);
-
-            // if($upload_image == 0){
-            //     return redirect()->back()->withInput()->with('error', 'Gagal mengupload gambar!');
-            // }
-
-            // $data['foto'] = $upload_image;
-
-        }catch(Exception $e){
-            Log::info($e->getMessage());
-            return redirect()->back()->withInput()->with('error', 'Gagal mengubah profile pengguna');
-        }
-
-        return redirect()->route('user.index')->with('success','Profile pengguna berhasil diubah!');
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
     }
 
-    public function destroy(User $user){
-        try{
-            $user->delete();
-        }catch(Exception $e){
-            Log::info($e->getMessage());
-            return response(['code' => 0, 'message' => 'Gagal menghapus user']);
-        }
-
-        return response(['code' => 1, 'message' => 'Berhasil menghapus user']);
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/datatable', function () {
         return view('example.datatable');
     });
-    
+
     Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
@@ -44,7 +44,7 @@ Route::middleware(['auth'])->group(function () {
     });
 
     Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
-        
+
         Route::group(['middleware' => 'role:admin'], function () {
             Route::get('create', [UserController::class, 'create'])->name('create');
             Route::post('store', [UserController::class, 'store'])->name('store');
@@ -58,49 +58,3 @@ Route::middleware(['auth'])->group(function () {
     });
 
 });
-
-
-Route::get('/forgot-password', function () {
-    return view('auth.forgot-password');
-})->middleware('guest')->name('password.request');
-
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
-
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
-
-    return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
-
-Route::get('/reset-password/{token}', function ($token) {
-    return view('auth.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
-Route::post('/reset-password', function (Request $request) {
-    $request->validate([
-        'token' => 'required',
-        'email' => 'required|email',
-        'password' => 'required|min:8|confirmed',
-    ]);
-
-    $status = Password::reset(
-        $request->only('email', 'password', 'password_confirmation', 'token'),
-        function ($user, $password) {
-            $user->forceFill([
-                'password' => Hash::make($password)
-            ])->setRememberToken(Str::random(60));
-
-            $user->save();
-
-            event(new PasswordReset($user));
-        }
-    );
-
-    return $status === Password::PASSWORD_RESET
-                ? redirect()->route('login')->with('status', __($status))
-                : back()->withErrors(['email' => [__($status)]]);
-})->middleware('guest')->name('password.update');
