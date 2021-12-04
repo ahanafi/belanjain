1a458492614e54d586c9e53bc6b50ec5c0363296
Ahmad Hanafi <Contaminated007@gmail.com>
2021-12-04 08:18:01 +0700

    fix: remove un-used method from Helpers

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,78 +1,9 @@
 <?php
-
-use Carbon\Carbon;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
-
-function formatPrice($value)
-{
-    return 'Rp ' . number_format($value, 0, ',', '.');
-}
-
-function authUser()
-{
-    return Auth::user();
-}
-
-function indonesianDate($date)
-{
-    return Carbon::parse($date)->isoFormat('LL');
-}
-
-function getAge($date)
-{
-    $birth_date = Carbon::parse($date);
-    $now = Carbon::now();
-
-    return $birth_date->diffInYears($now);
-}
-
-function getGender($gender)
-{
-    return $gender == 'lake' ? 'Laki-Laki' : 'Perempuan';
-}
-
-function getStatus($status)
-{
-    return $status == 1 ? '<span class="badge badge-primary">Aktif</span>' : '<span class="badge badge-secondary">Nonaktif</span>';
-}
-
-function uploadFile($base_64_foto, $folder)
-{
-    try {
-        $foto = base64_decode($base_64_foto['data']);
-        $folderName = 'images/' . $folder;
-
-        if (!file_exists($folderName)) {
-            mkdir($folderName, 0755, true);
-        }
-
-        $safeName = time() . $base_64_foto['name'];
-        $newPath = public_path() . '/' . $folderName;
-        file_put_contents($newPath . '/' . $safeName, $foto);
-
-    } catch (Exception $e) {
-        Log::info($e->getMessage());
-        return 0;
+if (!function_exists('isActive')) {
+    function isActive($param)
+    {
+        return request()->route()->getPrefix() === '/' . $param ? 'active' : '';
     }
-
-    return $folder . '/' . $safeName;
-}
-
-function isActive($param)
-{
-    return request()->route()->getPrefix() === '/' . $param ? 'active' : '';
-}
-
-function showFor($roles)
-{
-    foreach ($roles as $role) {
-        if (userRole() == $role) {
-            return true;
-        }
-    }
-
-    return false;
 }
 
 if (!function_exists('setFlashMessage')) {
@@ -105,8 +36,9 @@ if (!function_exists('setFlashMessage')) {
     }
 }
 
-if(!function_exists('removeDecimal')) {
-    function removeDecimal($number) {
+if (!function_exists('removeDecimal')) {
+    function removeDecimal($number)
+    {
         $number = explode(".", $number);
         return $number[0];
     }
