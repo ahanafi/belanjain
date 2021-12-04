d00ea47e704952077d574f1ee2c265eb221b2c67
Ahmad Hanafi <Contaminated007@gmail.com>
2021-12-04 08:11:59 +0700

    feat: add removeDecimal and flashMessage function

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,42 +1,47 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Request;
 
-function formatPrice($value){
-    return 'Rp '. number_format($value,0,',','.');
+function formatPrice($value)
+{
+    return 'Rp ' . number_format($value, 0, ',', '.');
 }
 
-function authUser(){
+function authUser()
+{
     return Auth::user();
 }
 
-function indonesianDate($date){
+function indonesianDate($date)
+{
     return Carbon::parse($date)->isoFormat('LL');
 }
 
-function getAge($date){
+function getAge($date)
+{
     $birth_date = Carbon::parse($date);
     $now = Carbon::now();
 
     return $birth_date->diffInYears($now);
 }
 
-function getGender($gender){
+function getGender($gender)
+{
     return $gender == 'lake' ? 'Laki-Laki' : 'Perempuan';
 }
 
-function getStatus($status){
+function getStatus($status)
+{
     return $status == 1 ? '<span class="badge badge-primary">Aktif</span>' : '<span class="badge badge-secondary">Nonaktif</span>';
 }
 
-function uploadFile($base_64_foto, $folder){
+function uploadFile($base_64_foto, $folder)
+{
     try {
         $foto = base64_decode($base_64_foto['data']);
-        $folderName = 'images/'.$folder;
+        $folderName = 'images/' . $folder;
 
         if (!file_exists($folderName)) {
             mkdir($folderName, 0755, true);
@@ -44,27 +49,65 @@ function uploadFile($base_64_foto, $folder){
 
         $safeName = time() . $base_64_foto['name'];
         $newPath = public_path() . '/' . $folderName;
-        file_put_contents($newPath. '/' . $safeName, $foto);
+        file_put_contents($newPath . '/' . $safeName, $foto);
 
     } catch (Exception $e) {
         Log::info($e->getMessage());
         return 0;
     }
 
-    return $folder.'/'.$safeName;
+    return $folder . '/' . $safeName;
 }
 
-function isActive($param){
-    return request()->route()->getPrefix() === '/'.$param ? 'active' : '';
+function isActive($param)
+{
+    return request()->route()->getPrefix() === '/' . $param ? 'active' : '';
 }
 
 function showFor($roles)
 {
-    foreach($roles as $role){
-        if(userRole() == $role){
+    foreach ($roles as $role) {
+        if (userRole() == $role) {
             return true;
         }
     }
 
     return false;
+}
+
+if (!function_exists('setFlashMessage')) {
+    function setFlashMessage($type, $actionType, $dataType)
+    {
+        $messageType = "";
+        $messageStatus = ($type === 'success') ? " berhasil " : " gagal ";
+
+        if (in_array($actionType, ['insert', 'add'])) {
+            $messageType = " disimpan!";
+        } else if (in_array($actionType, ['update', 'edit'])) {
+            $messageType = " diperbarui!";
+        } else if (in_array($actionType, ['delete', 'destroy', 'remove'])) {
+            $messageType = " dihapus!";
+        } else if ($actionType === 'upload') {
+            $messageType = " diupload!";
+        }
+
+        $messageText = "Data " . $dataType . $messageStatus . $messageType;
+
+        //For customize message
+        if ($actionType === 'custom') {
+            $messageText = $dataType;
+        }
+
+        return [
+            'type' => $type,
+            'text' => $messageText,
+        ];
+    }
+}
+
+if(!function_exists('removeDecimal')) {
+    function removeDecimal($number) {
+        $number = explode(".", $number);
+        return $number[0];
+    }
 }
